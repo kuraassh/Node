9a96d5365da0054a08e0295208357fef16e42185
danzilla0x <8739617+danzilla0x@users.noreply.github.com>
2022-08-03 00:38:11 +0300

    fix the order of gear node purge

--- Gear/update_gear.sh
+++ Gear/update_gear.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sudo systemctl stop gear
+$HOME/gear-node purge-chain -y
 wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz
 sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
 rm gear-nightly-linux-x86_64.tar.xz
-$HOME/gear-node purge-chain -y
-sudo systemctl restart gear
+sudo systemctl start gear
 echo "Обновление завершено успешно"
