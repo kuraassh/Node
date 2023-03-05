a1ddc997a3a81444c71b2b94c57afde563f5a4dc
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-03-05 18:17:26 +0200

    Update multitool.sh

--- massa/multitool.sh
+++ massa/multitool.sh
@@ -6,37 +6,43 @@ fi
 echo 'your discord: ' $DISCORD
 sleep 1
 
+echo "-----------------------------------------------------------------------------"
 echo "Generate new wallet"
-echo -e "${PINK}-----------------------------------------------------------------------------${NORMAL}"
+echo "-----------------------------------------------------------------------------"
 # Generate new wallet
 cd $HOME/massa/massa-client
 ./massa-client --pwd $massa_pass wallet_generate_secret_key
 
+echo "-----------------------------------------------------------------------------"
 echo "Echo wallet address"
-echo -e "${PINK}-----------------------------------------------------------------------------${NORMAL}"
+echo "-----------------------------------------------------------------------------"
 #Echo wallet address
 ADDRESS=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
 echo $ADDRESS
 
+echo "-----------------------------------------------------------------------------"
 echo "Echo wallet secret"
-echo -e "${PINK}-----------------------------------------------------------------------------${NORMAL}"
+echo "-----------------------------------------------------------------------------"
 #Echo wallet secret
 SECRET=$(./massa-client --pwd $massa_pass wallet_get_secret_key $ADDRESS | grep "S12")
 echo $SECRET
 
+echo "-----------------------------------------------------------------------------"
 echo "Add staking"
-echo -e "${PINK}-----------------------------------------------------------------------------${NORMAL}"
+echo "-----------------------------------------------------------------------------"
 #Add staking
 ./massa-client --pwd $massa_pass node_start_staking $ADDRESS
 
+echo "-----------------------------------------------------------------------------"
 echo "Registration to episode "
-echo -e "${PINK}-----------------------------------------------------------------------------${NORMAL}"
+echo "-----------------------------------------------------------------------------"
 #Registration to episode 
 REG=$(./massa-client --pwd $massa_pass node_testnet_rewards_program_ownership_proof $ADDRESS $DISCORD)
 echo $REG
 
+echo "-----------------------------------------------------------------------------"
 echo "Echo all data to file"
-echo -e "${PINK}-----------------------------------------------------------------------------${NORMAL}"
+echo "-----------------------------------------------------------------------------"
 #Echo all data to file 
 echo $ADDRESS $SECRET $REG > massa_20.txt
 
